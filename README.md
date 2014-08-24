b6aea8522fa5c2ff5955bfc7ce1480e388292b4f
Densaugeo <you@example.com>
2014-08-24 16:19:33 -0400

    Switched to Python

--- README.md
+++ README.md
@@ -3,8 +3,27 @@ LEDs
 
 LEDs dimmed by web interface through RPi!
 
-Might need to remove 'i2c-dev' or 'i2c-bcm2708' kernel module from autostart blacklist in /etc/modprobe.d/raspi-blacklist.conf
+Installation on Raspberry Pi:
 
-Installed: 
+# Might need to remove 'i2c-dev' or 'i2c-bcm2708' kernel module from autostart blacklist in /etc/modprobe.d/raspi-blacklist.conf
+sudo vi /etc/etc/modprobe.d/raspi-blacklist.conf
 
-sud apt-get install python-smbus
+# Install packages
+sudo apt-get install python-smbus python-pip
+sudo pip install tornado
+
+# Get the code and install npm dependencies
+git clone https://github.com/Densaugeo/LEDs
+
+# Start the server
+sudo python ./LEDs/server.py
+
+To start the server automatically when the Pi boots, add it to Cron:
+
+crontab -e
+
+And add the line '@reboot sudo python [install location]/LEDs/server.py'
+
+While the server is running, the PWM channels can be controlled by a simple web page at http://[Raspberry Pi IP]:8080/
+
+To my surprise, no firewall adjustments on the Pi were necessary to access this page
--- server.py
+++ server.py
@@ -7,9 +7,13 @@ import tornado.web
 import tornado.websocket
 from datetime import datetime
 import json
+from Adafruit_PWM_Servo_Driver import PWM
 
 def timestamp():
   return datetime.strftime(datetime.utcnow(), "%d %B %Y %I:%M:%S")
+
+pwm = PWM(0x40)
+pwm.setPWMFreq(1000)
 
 class MainHandler(tornado.web.RequestHandler):
   def prepare(self):
@@ -22,9 +26,30 @@ class ControlSocket(tornado.websocket.WebSocketHandler):
     print(timestamp() + ": Websocket opened")
   
   def on_message(self, message):
-    print(timestamp() + ": Received message: " + message)
+    try:
+      messageobject = json.loads(message)
+    except ValueError:
+      print("No valid JSON object")
+      return
     
-    messageobject = json.loads(message)
+    try:
+      pin = int(messageobject["pin"])
+      value = int(messageobject["value"])
+    except ValueError:
+      print("Pin or PWM value not a valid integer")
+      return
+    except AttributeError:
+      print("Pin or PWM value not defined")
+      return
+    
+    if (pin < 0 or pin > 15):
+      print("Pin number out of range")
+      return
+    if (value < 0 or value > 4095):
+      print("PWM value out of range")
+      return
+    
+    pwm.setPWM(pin, 0, value)
   
   def on_close(self):
     print(timestamp() + ": Websocket closed")
